01b4a78008e522ae195b0cd26e0057264cf7fe3f
Cynthia Kiser <cnk@caltech.edu>
2015-01-28 21:38:20 -0800

    sanity checking for subdomain names

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -36,4 +36,34 @@ feature "Accounts" do
     expect(page).to have_content("Sorry, your account could not be created.")
     expect(page).to have_content("Subdomain has already been taken")
   end
+
+  scenario "subdomain with restricted name" do
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Some Acct"
+    fill_in "Subdomain", :with => "ADMIN"
+    fill_in "Email", :with => "test@example.com"
+    fill_in "Password", :with => "password", :exact => true
+    fill_in "Password confirmation", :with => "password", :exact => true
+    click_button "Create Account"
+
+    expect(page.current_url).to eq('http://www.example.com/subscribem/accounts')
+    expect(page).to have_content("Sorry, your account could not be created.")
+    expect(page).to have_content("Subdomain is not allowed.")
+  end
+
+  scenario "subdomain with invalid characters" do
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Some Acct"
+    fill_in "Subdomain", :with => "<admin>"
+    fill_in "Email", :with => "test@example.com"
+    fill_in "Password", :with => "password", :exact => true
+    fill_in "Password confirmation", :with => "password", :exact => true
+    click_button "Create Account"
+
+    expect(page.current_url).to eq('http://www.example.com/subscribem/accounts')
+    expect(page).to have_content("Sorry, your account could not be created.")
+    expect(page).to have_content("Subdomain is not allowed.")
+  end
 end
